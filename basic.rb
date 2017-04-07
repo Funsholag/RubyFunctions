ba891170c3f90c319c3633559399bce8f395ad65
Olufunsho Olagundoye <Funsholag@gmail.com>
2017-04-07 13:48:27 -0400

    Initial Commit

---
+++ basic.rb
@@ -0,0 +1,34 @@
+# 1
+# def Hello(name1, name2)
+#   "Thank God " + name1 + name2
+# end
+#
+# puts (Hello("Donald Trump is ", "only In America."))
+
+#2
+# def counter(num)
+#   (1..100).each do |i|
+#     if i % 5 == 0
+#       puts "Buzz"
+#     elsif i % 3 == 0
+#       puts "Fizz"
+#     else
+#       puts i
+#     end
+#   end
+# end
+#
+# puts counter(10)
+
+# 3
+# def cities(city, state)
+#     result = {}
+#     for i in 0...(city.length)
+#         key = city[i]
+#         value = state[i]
+#         result[key] = value
+#     end
+#     result
+# end
+#
+# return cities ([:newyork, :boston, :miami]),([:newyork, :massachussetts, :florida])
